140b29abac209608befa221dc704bb8d6d1804f5
Max Veit <maxdveit@gmail.com>
2016-11-16 15:03:00 -0800

    Fixed and improved outlier plotting functions
    Modified especially the former escatter_outliers function - it now only
    plots the outliers and leaves user to make the central plot

--- plottools.py
+++ plottools.py
@@ -1,9 +1,12 @@
 """Plot tools: Reusable utilities for working with matplotlib plots.
 
 Contents:
-scatter_outliers    Make a scatterplot, marking outliers at the edges
 thin_points         Thin data points that are too close to each other
 scatter_thin_points Plot a set of thinned data points with weight info
+scatter_mark_outliers
+                    Mark outliers at the edges of a scatterplot
+scatter_outliers_size
+                    Make a scatterplot with sizes and outliers
 
 """
 
@@ -12,57 +15,6 @@ import matplotlib
 from matplotlib import pyplot
 import numpy as np
 from scipy.spatial import KDTree
-
-
-def escatter_outliers(xs, ys, xlim, ylim, ax=None, c_out='r', s_out=40,
-                      **plot_args):
-    """Make a scatterplot, marking outliers instead of cutting them off.
-
-    Paramters:
-    xs      List of x-coordinates of points, passed to pyplot.scatter()
-    ys      List of y-coordinates of points, passed to pyplot.scatter()
-    xlim    Limits of x-axis, as a tuple (xmin, xmax)
-    ylim    Limits of y-axis, as a tuple (ymin, ymax)
-    ax      Existing axis to plot into, if any
-    c_out   Colour to use to mark outliers, default 'r'
-    s_out   Size for outlier markers, default 40 (px)
-
-    Any other keyword arguments are passed to pyplot.scatter().
-
-    The outliers are marked at the edge of the graph as (typically)
-    larger red dots.  If a label was present, the outliers will be
-    labelled as the same with " (outliers)" appended.
-
-    """
-
-    if ax is None:
-        ax = pyplot.gca()
-    plot_args['linewidth'] = 0
-    ax.scatter(xs, ys, **plot_args)
-    outl_top = xs[(ys > ylim[1]) & (xs >= xlim[0]) & (xs <= xlim[1])]
-    outl_bot = xs[(ys < ylim[0]) & (xs >= xlim[0]) & (xs <= xlim[1])]
-    # Let out_left and outl_right handle the (literal) corner cases
-    outl_left = ys[(xs < xlim[0])]
-    outl_right = ys[(xs > xlim[1])]
-    outl_left[ys < ylim[0]] = ylim[0]
-    outl_left[ys > ylim[1]] = ylim[1]
-    outl_right[ys < ylim[0]] = ylim[0]
-    outl_right[ys > ylim[1]] = ylim[1]
-    plot_args['c'] = c_out
-    plot_args['s'] = s_out
-    plot_args['alpha'] = 0.8
-    plot_args['clip_on'] = False
-    plot_args['label'] = (plot_args['label'] + ' (outliers)'
-                          if 'label' in plot_args else '')
-    escatter(outl_top, ylim[1] * np.ones_like(outl_top), ax=ax, **plot_args)
-    plot_args['label'] = '_'
-    escatter(outl_bot, ylim[0] * np.ones_like(outl_bot), ax=ax, **plot_args)
-    escatter(xlim[0] * np.ones_like(outl_left), outl_left, ax=ax, **plot_args)
-    escatter(xlim[1] * np.ones_like(outl_right), outl_right, ax=ax, **plot_args)
-    ax.set_xlim(*xlim)
-    ax.set_ylim(*ylim)
-    # TODO would be useful to return the collection of plots instead
-    return None
 
 
 def thin_points(data, density=None, len_scale=0.01, nmax=None, r=None):
@@ -167,4 +119,89 @@ def scatter_thin_points(data_thin, weights, method='size', ax=None,
     else:
         raise ValueError("Unknown representation method " + method)
     return pl
+
+
+def scatter_mark_outliers(xs, ys, xlim, ylim, ax=None, **plot_args):
+    """Mark outliers at the edges of a scatterplot
+
+    Paramters:
+    xs      List of x-coordinates of points, passed to pyplot.scatter()
+    ys      List of y-coordinates of points, passed to pyplot.scatter()
+    xlim    Limits of x-axis, as a tuple (xmin, xmax)
+    ylim    Limits of y-axis, as a tuple (ymin, ymax)
+    ax      Existing axis to plot into, if any
+
+    Any other keyword arguments are passed to pyplot.scatter().
+
+    The outliers are marked at the edge of the graph as large red dots,
+    by default size 40, color 'r'.  If a label was present, the outliers
+    will be labelled as the same with " (outliers)" appended.
+
+    Returns the collection of scatterplots - (top, bottom, left, right).
+
+    """
+
+    if ax is None:
+        ax = pyplot.gca()
+    outl_top = xs[(ys > ylim[1]) & (xs >= xlim[0]) & (xs <= xlim[1])]
+    outl_bot = xs[(ys < ylim[0]) & (xs >= xlim[0]) & (xs <= xlim[1])]
+    # Let out_left and outl_right handle the (literal) corner cases
+    outl_left = np.array(ys[(xs < xlim[0])])
+    outl_right = np.array(ys[(xs > xlim[1])])
+    outl_left[outl_left < ylim[0]] = ylim[0]
+    outl_left[outl_left > ylim[1]] = ylim[1]
+    outl_right[outl_right < ylim[0]] = ylim[0]
+    outl_right[outl_right > ylim[1]] = ylim[1]
+    if 'linewidth' not in plot_args:
+        plot_args['linewidth'] = 0
+    if 'c' not in plot_args:
+        plot_args['c'] = 'r'
+    if 's' not in plot_args:
+        plot_args['s'] = 40
+    plot_args['clip_on'] = False
+    plot_args['label'] = (plot_args['label'] + ' (outliers)'
+                          if 'label' in plot_args else '_')
+    op_t = ax.scatter(outl_top, ylim[1] * np.ones_like(outl_top), **plot_args)
+    plot_args['label'] = '_'
+    op_b = ax.scatter(outl_bot, ylim[0] * np.ones_like(outl_bot), **plot_args)
+    op_l = ax.scatter(
+        xlim[0] * np.ones_like(outl_left), outl_left, **plot_args)
+    op_r = ax.scatter(
+        xlim[1] * np.ones_like(outl_right), outl_right, **plot_args)
+    # Not typically necessary, but just ensure the outlier markers end
+    # up at the edge of the plot
+    ax.set_xlim(*xlim)
+    ax.set_ylim(*ylim)
+    return op_t, op_b, op_l, op_r
+
+
+def scatter_outliers_size(data_thin, weights, xlim, ylim, ax=None, c_out='r',
+                          s_out=40, **plot_args):
+    """Do scatterplot with outliers, encoding the given weights as size.
+
+    NB This doesn't change the size of the outlier markers - it might be
+    possible by modifying the original escatter_outliers() function, but
+    does it even make sense to encode the weights of outliers?
+
+    Parameters:
+    data_thin   Thinned data, e.g. from thin_points()
+    weights     Weights of the thinned data, e.g. from thin_points()
+                (represents how many real points each thin point
+                represents
+    xlim        Limits of x-axis, as a tuple (xmin, xmax)
+    ylim        Limits of y-axis, as a tuple (ymin, ymax)
+    ax          Existing axis to plot into, if any
+    c_out       Colour to use to mark outliers, default 'r'
+    s_out       Size for outlier markers, default 40 (px)
+    Any remaining keyword arguments will only modify the properties of
+    the central (non-outlier) scatterplot.
+
+    Returns the original plot as well as the four outlier scatterplots -
+    (orig, top, bottom, left, right)
+
+    """
+    pl = scatter_thin_points(data_thin, weights, ax=ax)
+    outps = scatter_mark_outliers(
+        *data_thin.T, xlim=xlim, ylim=ylim, ax=ax, c=c_out, s=s_out)
+    return (pl,) + outps
 

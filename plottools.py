102efd8361c5f7fcf9ac662a2479e0c9e5b28cf9
Max Veit <maxdveit@gmail.com>
2017-04-06 16:47:09 +0100

    Fixed naming issue

--- plottools.py
+++ plottools.py
@@ -11,8 +11,8 @@ scatter_outliers_size
 """
 
 
-import matplotlib
-from matplotlib import pyplot
+import matplotlib as mpl
+from mpl import pyplot
 import numpy as np
 from scipy.spatial import KDTree
 
